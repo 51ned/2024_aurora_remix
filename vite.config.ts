67d9aca29a187dcc06b8c0336a9a65b687de8796
Denis <sustain@bk.ru>
2024-05-17 17:22:16 +0300

    basic settings # 2.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,47 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-import { installGlobals } from '@remix-run/node'
 import { vitePlugin as remix } from '@remix-run/dev'
 
 import browserslist from 'browserslist'
 import { browserslistToTargets } from 'lightningcss'
 
-// import postcssModules from 'postcss-modules'
+
+type ViteConfigInput = {
+  command: string,
+  mode: string
+}
 
 
-installGlobals()
+export default (args: ViteConfigInput) => {
+  const generateScopedName = args.mode === 'development'
+    ? ``
+    : '[hash:base64:3]'
 
-
-export default defineConfig({
-  build: {
-    cssMinify: 'lightningcss'
-  },
-
-  css: {
-    lightningcss: {
-      targets: browserslistToTargets(browserslist('>= 0.25%'))
+  return defineConfig({
+    build: {
+      cssMinify: 'lightningcss'
     },
-    // postcss: {
-    //   plugins: [
-    //     postcssModules({
-    //       generateScopedName: '[hash:base64:8]'
-    //     })
-    //   ]
-    // },
-    transformer: 'lightningcss'
-  },
-
-  plugins: [
-    remix(),
-    tsconfigPaths()
-  ],
   
-  server: {
-    headers: {
-      'accept-ch': 'sec-ch-prefers-color-scheme'
+    css: {
+      lightningcss: {
+        targets: browserslistToTargets(browserslist('>= 0.25%'))
+      },
+      modules: {
+        generateScopedName
+      },
+      transformer: 'lightningcss'
+    },
+  
+    plugins: [
+      remix(),
+      tsconfigPaths()
+    ],
+    
+    server: {
+      headers: {
+        'accept-ch': 'sec-ch-prefers-color-scheme'
+      }
     }
-  }
-})
+  })
+}
