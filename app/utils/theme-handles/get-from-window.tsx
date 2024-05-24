2794ae9e009e165bf92a45dacafd72f34f987577
51ned <sustain@bk.ru>
2024-05-24 18:36:33 +0300

    migration # 4.7

--- app/utils/theme-handles/get-from-window.tsx
+++ app/utils/theme-handles/get-from-window.tsx
@@ -3,20 +3,16 @@ import { useEffect, useState } from 'react'
 import { setCookies } from './set-cookies'
 
 
-export function getFromWindow(): string {
-  const [theme, setTheme] = useState('')
+export function getFromWindow() {
+  const [theme, setTheme] = useState(() => {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  })
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const windowTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'dark'
-        : 'light'
-
-      setTheme(windowTheme)
-      setCookies(windowTheme)
+      setCookies(theme)
     }
-  }, [])
+  }, [theme])
 
   return theme
 }
-
