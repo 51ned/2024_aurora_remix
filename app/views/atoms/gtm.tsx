5d3ae2cd98d847259649f4376e3227d6aaf0e117
51ned <sustain@bk.ru>
2024-05-27 15:58:08 +0300

    migration # 6.3.2

--- app/views/atoms/gtm.tsx
+++ app/views/atoms/gtm.tsx
@@ -7,25 +7,30 @@ import * as gtag from 'utils/gtm-handle'
 export function GTM({ gtmId }: { gtmId: string | undefined }) {
   const location = useLocation()
   const scriptId = useId()
+  let gtmScript: HTMLScriptElement
 
   useEffect(() => {
     if (process.env.NODE_ENV !== 'development' || gtmId?.length) {
-      const gtmScript = document.createElement('script')
+      if (!document.getElementById(scriptId)) {
+        gtmScript = document.createElement('script')
 
-      gtmScript.innerHTML = `
-        (function(w,d,s,l,i){
-          w[l]=w[l]||[];
-          w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-          var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),
-              dl=l!='dataLayer'?'&l='+l:'';
-          j.async=true;
-          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${gtmId}');
-      `
+        gtmScript.defer = true
+        gtmScript.id = scriptId
+        gtmScript.innerHTML = `
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtmId}');
+        `
 
-      document.head.appendChild(gtmScript)
+        document.head.appendChild(gtmScript)
+      }
 
       return () => {
         document.head.removeChild(gtmScript)
@@ -38,42 +43,6 @@ export function GTM({ gtmId }: { gtmId: string | undefined }) {
   //     gtag.pageview(location.pathname, gtmId)
   //   }
   // }, [location, gtmId])
-
-  // if (process.env.NODE_ENV === 'development' || !gtmId) {
-  //   return null
-  // }
-  
-  // return (
-  //   <>
-  //     <noscript>
-  //       <iframe
-  //         height='0'
-  //         src={`https://www.googletagmanager.com/ns.html?id=`}
-  //         style={{ display: 'none', visibility: 'hidden' }}
-  //         width='0'
-  //       />
-  //     </noscript>
-            
-  //     <script
-  //       async
-  //       dangerouslySetInnerHTML={{
-  //         __html: `
-  //           (function(w,d,s,l,i){
-  //             w[l]=w[l]||[];
-  //             w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-  //             var f=d.getElementsByTagName(s)[0],
-  //                 j=d.createElement(s),
-  //                 dl=l!='dataLayer'?'&l='+l:'';
-  //             j.async=true;
-  //             j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-  //             f.parentNode.insertBefore(j,f);
-  //           })(window,document,'script','dataLayer','${gtmId}');
-  //         `
-  //       }}
-  //       id={scriptId}
-  //     />
-  //   </>
-  // )
 
   return process.env.NODE_ENV === 'development' || !gtmId ? null : (
     <>
