b93f165e858e9978117e28e77ceef5bdeb32d4f8
51ned <sustain@bk.ru>
2024-05-28 17:15:19 +0300

    gtm optimisation, pt. # 5

--- app/views/atoms/gtm.tsx
+++ app/views/atoms/gtm.tsx
@@ -3,47 +3,43 @@ import { useLocation } from '@remix-run/react'
 
 import * as gTag from 'utils/gtm-handle'
 
+
 export function GTM({ gtmId }: { gtmId: string | undefined }) {
   const location = useLocation()
   const scriptId = 'gtm'
-
+  
   useEffect(() => {
     if (process.env.NODE_ENV === 'production' && gtmId?.length) {
-      const loadGTM = () => {
-        if (!document.getElementById(scriptId)) {
-          const gtmScript = document.createElement('script')
+      if (!document.getElementById(scriptId)) {
+        const gtmScript = document.createElement('script')
 
-          gtmScript.async = true
-          gtmScript.fetchPriority = 'low'
-          gtmScript.id = scriptId
-
-          gtmScript.innerHTML = `
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),
-                  dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${gtmId}');
-          `
-
-          document.head.appendChild(gtmScript)
-          gtmScript.onload = () => gTag.pageview(location.pathname, gtmId)
-        }
+        gtmScript.async = true
+        gtmScript.fetchPriority = 'low'
+        gtmScript.id = scriptId
         
-        else {
-          gTag.pageview(location.pathname, gtmId)
-        }
-      }
+        gtmScript.innerHTML = `
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtmId}');
+        `
 
-      window.addEventListener('load', loadGTM)
-      return () => window.removeEventListener('load', loadGTM)
+        document.head.appendChild(gtmScript)
+        gtmScript.onload = () => gTag.pageview(location.pathname, gtmId)
+      }
+      
+      else {
+        gTag.pageview(location.pathname, gtmId)
+      }
     }
   }, [location, gtmId])
-
+  
   return process.env.NODE_ENV !== 'development' || gtmId?.length ? null : (
     <>
       <noscript>
